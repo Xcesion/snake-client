74687cc664f67af627150c37432a924948ac0e50
Frank Shen <frank851207@gmail.com>
2023-06-08 12:38:04 -0700

    added handleuserInput function and it's callback for stdin

--- play.js
+++ play.js
@@ -1,6 +1,29 @@
 const net = require("net");
 const {connect} = require("./client");
 
+// const handleUserInput = function () {
+//   // your code here
+// };
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = stdin.on("data", key => {
+    const stdout = process.stdout;
+    if (key === '\u0003') {
+      stdout.write("Exited the game.\n");
+      process.exit();
+    }
+  });
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
 
